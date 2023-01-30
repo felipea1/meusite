02a0d72d969649cab9f4c073b39016872d5b1822
felipea1 <104094703+felipea1@users.noreply.github.com>
2023-01-30 16:44:31 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,8 +1,14 @@
-<html>
-<head>
-<title>Teste PHP</title>
-</head>
-<body>
-<?php echo "<p>Olá Mundo</p>"; ?>
-</body>
-</html>
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP por Felipe</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Felipe Duarte<br>';
+				echo 'YouTube: https://www.youtube.com/channel/UC1e3Muh9VUtdEPL1kyIfcUw<br>'; 
+			?>
+		</body>
+	</html>
